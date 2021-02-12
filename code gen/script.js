aff5155eb9a593f5e9e092d525325796569f8d17
jvilla <josevillacinda96@gmail.com>
2021-02-11 19:02:51 -0800

    corrected typo in README and removed comments

--- code gen/script.js
+++ code gen/script.js
@@ -1,17 +1,3 @@
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
 document.getElementById('generate').addEventListener('click', () => {
 
   let length = prompt('Input desired amount of characters in your password. (From 8-128)')
